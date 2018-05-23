c5f9cefd43ec276f673b9508982cb1b9c804e0d6
Anissa Wong <aywong40@gmail.com>
2018-05-23 14:06:08 -0700

    Update map2.py

--- map2.py
+++ map2.py
@@ -5,6 +5,11 @@ import requests
 import math
 import Tkinter
 import webbrowser
+import urllib
+import PIL
+from PIL import Image,ImageTk
+from io import BytesIO
+import base64
 
 sqrt = math.sqrt
 atan2 = math.atan2
@@ -39,18 +44,22 @@ coordinates = [v,u]
 width = 640
 height = 480
 zoom = 13
-window = Tk()
-window.title("etherwhere")
-window.minsize(width, height)
-preq = requests.PreparedRequest()
-preq.prepare_url(GPS_url, {'center':'coordinates', 'size':'800x500'})
-mapimage = webbrowser.open(preq.url)
-canvas.create_image(0,0,image=mapimage,anchor=Tkinter.NW)
-canvas.pack()
-window.mainloop()
+str_coor = "%.7f,%.7f" % (v,u)
+preq= requests.PreparedRequest()
+preq.prepare_url(GPS_url, {'center':str_coor, 'size':'800x500', 'zoom':zoom})
+print(preq.url)
 
-canvas.pack()
-window.mainloop()
+master = Tkinter.Tk()
+master.title("etherwhere")
+#master.minsize(width, height)
+urlreader = urllib.urlopen(preq.url)
+imgdata = urlreader.read()
+urlreader.close()
+base64data = base64.encodestring(imgdata)
+image = Tkinter.PhotoImage(data=base64data)
+w = Tkinter.Canvas(master, width =width, height= height)
+w.create_image(0,0, image=image, anchor=Tkinter.NW)
+w.pack()
+master.mainloop()
 
-print(u)
-print(v)
+
